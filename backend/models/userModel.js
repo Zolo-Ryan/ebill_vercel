aff7097eadc4de65c751610886132a67feec4ff7
Utsav631 <12212034@nitkkr.ac.in>
2024-03-19 12:21:24 +0530

    backend setup 2

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name:{
         type: String,
+        required: [true, "Please add a name"]
+    },
+    email: {
+        type: String,
         required: [true, "Please add a name"],
         unique: true,
         trim : true,
