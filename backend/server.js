28b30719855bf935619e92a11689a6cb3ccaae22
amandehmiwal <asdehmiwal8888@gmail.com>
2024-03-25 21:10:28 +0530

    4:00:00, added some basic contollers and routes

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // const cors = require('cors');
-const { default: mongoose } = require('mongoose');
-require('dotenv').config()
+const { default: mongoose } = require("mongoose");
+require("dotenv").config();
 const userRoute = require("./routes/userRoute");
-const errorHandler = require('./middleware/errorMiddleware');
-
+const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 // Mongo DB Connections
-mongoose.connect(process.env.MONGO_DB_URL, {
+mongoose
+  .connect(process.env.MONGO_DB_URL, {})
+  .then((response) => {
+    console.log("MongoDB Connection Succeeded.");
+  })
+  .catch((error) => {
+    console.log("Error in DB connection: " + error);
+  });
 
-}).then(response=>{
-    console.log('MongoDB Connection Succeeded.')
-}).catch(error=>{
-    console.log('Error in DB connection: ' + error)
-});
-
- 
 // Middleware Connections
- 
-
-
 
 // app.use(cors())
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.urlencoded({extended: false}))
-// app.use(bodyParser.json()) 
+app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.json())
 
 //Routes Middleware
 app.use("/api/users", userRoute);
 
-
 // Routes
 app.get("/", (req, res) => {
-    res.send("Home Page");
-})
+  res.send("Home Page");
+});
 
 //error middleware
 app.use(errorHandler);
 
-
 // Connection
-const PORT = process.env.PORT || 3000
-app.listen(PORT, ()=>{
-    console.log('App running in port: '+PORT)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("App running in port: " + PORT);
+});
